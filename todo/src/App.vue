74b1a9d7a632bba14fef97563897e23e36291702
Lee <dltpdnd1234@gmail.com>
2021-03-22 14:45:26 -0500

    todo v-for and props sets

---
+++ todo/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app" class="container">
+    <h1 class="text-center">Todo App</h1>
+    <input type="text" class="w-100 p-2" placeholder="Type todo">
+    <hr>
+    <Todo
+    v-for="todo in todos"
+    :key="todo.id"
+    :todo="todo"
+    />
+  </div>
+</template>
+
+<script>
+import Todo from '@/components/Todo.vue';
+export default {
+  components: {
+    Todo
+  },
+  data(){
+    return{
+      todos: [
+        {id: 1, text: 'buy a car', checked:false},
+        {id: 2, text: 'play games', checked:false},
+      ]
+    }
+  }
+
+}
+</script>
