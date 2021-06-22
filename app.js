7d312e44a1f6f9b8bc64cea6ecda903073565eb2
Lee <dltpdnd1234@gmail.com>
2021-06-22 18:13:06 -0500

    vue bind v-for @mouse

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
+
 const app = Vue.createApp({
   data(){
     return{
+      url:'https://www.google.com',
       showbooks:true,
-      title:'The Final',
-      author:'LEE',
-      age:25
+      books:[
+        {title:'LEE1', author:'LLEE1', img:'/images/1.png'},
+        {title:'LEE2', author:'LLEE2', img:'/images/2.png'},
+        {title:'LEE3', author:'LLEE3', img:'/images/3.png'}
+      ],
+      x:0,
+      y:0
     }
   },
   methods: {
     toggleShowBooks(){
       this.showbooks = !this.showbooks
+    },
+    handleEvent(e,data){
+      console.log(e, e.type)
+      if(data){
+        console.log(data)
+      }
+    },
+    handleMousemove(e){
+      this.x = e.offsetX
+      this.y = e.offsetY
     }
   },
 })
