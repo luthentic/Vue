e60dc5ec9a098a76fa103b4f5d78f2ab5da8313b
Lee <dltpdnd1234@gmail.com>
2021-06-24 23:22:49 -0500

    v-model

--- lee/src/App.vue
+++ lee/src/App.vue
@@ -1,51 +1,16 @@
 <template>
-  <div>
-    <h1>{{title}}</h1>  
-    <teleport to='.modals'  v-if="showModal">
-      <Modal theme="sale" @close="toggleModal">
-        <template v-slot:links>
-          <a href="!">Sign up</a>
-          <a href="#">More Info</a>
-        </template>
-        <h1>LEE WELCOME</h1>
-        <p>Grab Your hands</p>
-      </Modal>
-    </teleport>
-    <teleport to='.modals' v-if="showModal2">
-      <Modal theme="sale" @close="toggleModal2">
-        <h1>LEE WELCOME222</h1>
-        <p>Grab Your hands22</p>
-      </Modal>
-    </teleport>  
-  </div>
-  <button @click="toggleModal">Open Modal</button>
-  <button @click='toggleModal2'>Open Modal2</button>
+  <SingupForm/>
 </template>
 
 
 <script>
-import Modal from './components/Modal.vue'
+import SingupForm from './components/SingupForm.vue'
+
 
 export default {
   name: 'App',
-  components:{Modal},
-  data(){
-    return {
-      title:'LEE VUE PAGE',
-      header:"SIGN UP FOR THE GIVEAWAY",
-      text:"LEEEEEE",
-      showModal:false,
-      showModal2:false
-    }
-  },
-  methods: {
-    toggleModal(){
-      this.showModal = !this.showModal
-    },
-    toggleModal2(){
-      this.showModal2 = !this.showModal2
-    }
-  }
+  components:{SingupForm},
+
 }
 </script>
 
@@ -58,10 +23,8 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-h1{
-  border-bottom: 1px solid #ddd;
-  display: inline-block;
-  padding-bottom: 10px;
+body {
+  margin: 0;
+  background: #eee;
 }
-
 </style>
